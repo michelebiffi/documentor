a9075cd201a8574db0c225aec62d50afed96b52d
michelebiffi <michelebiffi@sorint.com>
2024-08-06 22:03:45 +0200

    Added authentication with Github/Google using Clerk

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,15 +1,26 @@
 import { Editor } from '@/components/editor/Editor';
 import { Header } from '@/components/Header'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 
 const Document = () => {
     return (
         <div>
             <Header>
                 <div className="flex w-fit items-center justify-center gap-2">
-                    <p className="document-title">
-                        Fake document title
-                    </p>
+                    <p className="document-title">Share</p>
                 </div>
+
+            {/* Sign out */}
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+
+            {/* Sign in  */}
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+
+
             </Header>
 
             <Editor />
